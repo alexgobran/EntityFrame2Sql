6cd9424e63bcc1dbd34b92cc09cdf82f260df355
alexgobran <54282581+alexgobran@users.noreply.github.com>
2019-09-11 16:38:27 -0400

    2nd commit

---
+++ EntityFrame2SqlLib/UsersRepository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace EntityFrame2SqlLib {
+    public class UsersRepository {
+
+        private static PRSContext context = new PRSContext();
+
+        public static List<User> GetAll() {
+            
+            return context.User.ToList();
+           
+            
+        
+
+
+        }
+
+        public static User GetByPK(int id) {
+            if(id<1) { throw new Exception("User id must be greater than 0"); }
+            return context.User.Find(id);
+            
+        }
+
+        public static bool Insert(User user) {
+            
+            if(user==null) { throw new Exception("User instance must not be null"); }
+            user.Id = 0;
+            context.User.Add(user);
+            return context.SaveChanges() == 1;
+
+        }
+
+        public static bool Update(User user) {
+            if (user == null) { throw new Exception("User instance must not be null"); }
+            var dbuser= context.User.Find(user.Id);
+            if (dbuser== null) { throw new Exception("No User with that Id"); }
+            dbuser.Username = user.Username;
+            dbuser.Password = user.Password;
+
+                       
+            return context.SaveChanges() == 1;
+            
+            throw new NotImplementedException();
+        }
+
+        public static bool Delete(User user) {
+            if (user == null) { throw new Exception("User instance must not be null"); }
+            var dbuser = context.User.Find(user.Id);
+            if (dbuser == null) { throw new Exception("No User with that Id"); }
+            context.User.Remove(dbuser);
+
+
+            return context.SaveChanges() == 1;
+            throw new NotImplementedException();
+        }
+
+        public static bool Delete(int id) {
+            var user = context.User.Find(id);
+            if(user== null) { return false; }
+            var rc = Delete(user);
+            return rc;
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
--- EntityFrame2Sqlproj/Program.cs
+++ EntityFrame2Sqlproj/Program.cs
@@ -7,29 +7,79 @@ namespace EntityFrame2Sqlproj {
         static void Main(string[] args) {
             //always reference the context instance in EF
             
-            using (var context = new PRSContext()) {
+            using(var context = new PRSContext()) {
+
+
+                //var request = new Request() {
+                //    Id = 0,
+                //    Description = "A New Request",
+                //    Justification = "I don't need one!",
+                //    RejectionReason = null,
+                //    DeliveryMode = "Pickup",
+                //    Status = "NEW",
+                //    Total = 0,
+                //    //bring back a user instance but only within the id column
+                //    UserId = context.User.SingleOrDefault(u => u.Username.Equals("george1")).Id
+
+                //context.Request.Add(request);
+
+                //var request = new Request () { Id = 3, Description = "Another Changed Description"};
+                //var dbRequest = context.Request.Find(request.Id);
+                //dbRequest.Description = request.Description;
+
+                //dbRequest = context.Request.Find(3);
+                ////when removing make sure they are no FK's point to what you're deleting or will get an exception error
+                //context.Request.Remove(dbRequest);
+                //context.SaveChanges();
+
+
+                //adds to collection but doesnt change the database in sql
+                //var Dellrequest = context.Request.Find(1);
+                //Dellrequest.Total = Dellrequest.RequestLine.Sum(l => l.Product.Price * l.Quantity);
+                //Console.WriteLine($"{Dellrequest.Description} {Dellrequest.Status} { Dellrequest.Total}");
+
+                //Dellrequest.RequestLine.ToList().ForEach(r1 =>
+                //{
+                //    Console.WriteLine($"{r1.Product.Name,-10} {r1.Quantity,5} " +
+                //        $"{r1.Product.Price.ToString("C"),10} " +
+                //        $"{(r1.Product.Price * r1.Quantity).ToString("C"),11}");
+
+
+                //});
+                //context.SaveChanges();
+                var total = context.Request.Sum(r => r.Total);
+                Console.WriteLine($"Total all requests is {total}");
+
+
+
+
+
                 //takes all instances in Vendor table and puts in array
                 //Find is how u read tables by primary key
-                var samsung = context.Vendor.Find(1);
+                //var samsung = context.Vendor.Find(1);
 
-                Console.WriteLine($"{samsung.Code} {samsung.Name}");
+                //Console.WriteLine($"{samsung.Code} {samsung.Name}");
 
                 //Lambda syntax
                 //vendors.ForEach(v => Console.WriteLine($"{v.Code} {v.Name}"));
 
-                var users = context.User.ToList();
+                //var users = context.User.ToList();
 
-            
+                //context can be called anything - it is the instanc of the dbuser
+                //var users = from u in context.User.ToList()
+                //            where u.Username.Contains("1") || u.Username.Contains("2")
+                //select u;
 
-            //    //query syntax
-            ////    foreach (var user in users)
-            ////    {
-            ////    Console.WriteLine($"{user.Username} {user.Firstname} {user.Lastname}");
-            ////      }
-
+                //    //query syntax
+                //    foreach (var user in users)
+                //    {
+                //        Console.WriteLine($"{user.Username} {user.Firstname} {user.Lastname}");
+                //    }
             }
 
-            
         }
+
+
+        
     }
 }
